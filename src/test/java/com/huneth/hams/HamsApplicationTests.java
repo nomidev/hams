ca99ce8a0163a2bcaccaf845f02e50588b4a3dc1
nomi <nomisoul@gmail.com>
2021-01-21 10:09:54 +0900

    init

---
+++ src/test/java/com/huneth/hams/HamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huneth.hams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
