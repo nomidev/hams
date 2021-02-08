75e4213fe674c08bf1c3aea72985c7cd2498e8c8
nomi <nomisoul@gmail.com>
2021-02-08 22:21:42 +0900

    20210208

--- src/main/java/com/huneth/hams/handler/GlobalExceptionHandler.java
+++ src/main/java/com/huneth/hams/handler/GlobalExceptionHandler.java
@@ -1,16 +1,23 @@
 package com.huneth.hams.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(value = Exception.class)
-    public String handler(Exception e, Model model) {
-        e.getMessage();
-        e.getCause();
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    public String handler(Model model, Exception e, HttpStatus httpStatus) {
+        HttpStatus status = httpStatus.INTERNAL_SERVER_ERROR;
+        log.error(e.getMessage());
+
+        model.addAttribute("erroMessage", e.getMessage());
+        model.addAttribute("erroMessage", httpStatus.getClass();
 
         return "error/error";
     }
