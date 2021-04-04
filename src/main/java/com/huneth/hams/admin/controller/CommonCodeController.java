52997183181178d0b36cf3f612e23209f0b23295
nomi <nomisoul@gmail.com>
2021-04-04 23:00:48 +0900

    20210404

--- src/main/java/com/huneth/hams/admin/controller/CommonCodeController.java
+++ src/main/java/com/huneth/hams/admin/controller/CommonCodeController.java
@@ -5,20 +5,19 @@ import com.huneth.hams.admin.service.CommonCodeService;
 import com.huneth.hams.common.dto.ResponseDto;
 import com.huneth.hams.common.commonEnum.StatusEnum;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @Controller
 @RequestMapping("/admin")
 public class CommonCodeController {
@@ -37,6 +36,28 @@ public class CommonCodeController {
     @RequestMapping("/commonCode/api")
     @ResponseBody
     public ResponseEntity<ResponseDto> cmmcList(Model model) {
+        List<CommonCode> commonCodeList = commonCodeService.retrieveCommonCodeList();
+
+        // Toast UI Grid는 contents에 데이터를 담아야 한다.
+        Map result = new HashMap();
+        result.put("contents", commonCodeList);
+
+        ResponseDto responseDto = ResponseDto.builder()
+                .data(result)
+                .message("SUCCESS")
+                .statusCode(StatusEnum.OK)
+                .result(true)
+                .build();
+
+        return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.OK);
+    }
+
+    @PostMapping("/commonCode/api")
+    @ResponseBody
+    public ResponseEntity<ResponseDto> save(@RequestBody Map<String, Object> param) {
+        log.info("param = " + param);
+
+
         List<CommonCode> commonCodeList = commonCodeService.retrieveCommonCodeList();
 
         // Toast UI Grid는 contents에 데이터를 담아야 한다.
